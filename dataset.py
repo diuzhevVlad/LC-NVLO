28697e5df004eb1c7d43fed96461ca15023fdd27
Vladislav Diuzhev <vladdiuzhev@gmail.com>
2025-03-23 13:26:16 +0300

    loading all data

--- dataset.py
+++ dataset.py
@@ -1,11 +1,9 @@
 import os
-import torch
-import pandas as pd
-from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torch.utils.data import Dataset
+from typing import List
+import skimage.io as io
+from odom_utils import get_calibration
 
 # Ignore warnings
 import warnings
@@ -20,8 +18,10 @@ class KittiVLOdomDataset(Dataset):
         self,
         root_dir: str,
         calib_filename: str = "calib.txt",
-        diff_gt_poses_filename="diff_gt_poses.npy",
-        diff_kiss_poses_filename="diff_kiss_poses.npy",
+        diff_gt_poses_filename: str = "diff_gt_poses.npy",
+        diff_kiss_poses_filename: str = "diff_kiss_poses.npy",
+        sequences_to_load: List[str] = [str(j).zfill(2) for j in range(11)],
+        annotated: bool = True,
     ):
         """
         Arguments:
@@ -32,6 +32,14 @@ class KittiVLOdomDataset(Dataset):
         # Initialize sequences information
         seq_path = os.path.join(root_dir, "sequences")
         seq_names = os.listdir(seq_path)
+
+        if sequences_to_load is not None:
+            if not set(sequences_to_load).issubset(seq_names):
+                raise RuntimeError(
+                    f"Not all sequences to load exist: {set(sequences_to_load).difference(seq_names)}!"
+                )
+            seq_names = sequences_to_load
+
         self.seqs_info = {
             seq: {
                 prop: None
@@ -40,16 +48,16 @@ class KittiVLOdomDataset(Dataset):
                     "pcd_path",
                     "img0_path",
                     "img1_path",
-                    "calib_path",
-                    "diff_gt_poses_path",
-                    "diff_kiss_poses_path",
+                    "calib",
+                    "diff_gt_poses",
+                    "diff_kiss_poses",
                 ]
             }
             for seq in seq_names
         }
 
         # Initialize sequences information
-        for _seq_name in seq_names:
+        for _seq_name in sorted(seq_names):
             self.seqs_info[_seq_name]["pcd_path"] = os.path.join(
                 seq_path, _seq_name, "velodyne"
             )
@@ -58,15 +66,6 @@ class KittiVLOdomDataset(Dataset):
             )
             self.seqs_info[_seq_name]["img1_path"] = os.path.join(
                 seq_path, _seq_name, "image_1"
-            )
-            self.seqs_info[_seq_name]["calib_path"] = os.path.join(
-                seq_path, _seq_name, calib_filename
-            )
-            self.seqs_info[_seq_name]["diff_gt_poses_path"] = os.path.join(
-                seq_path, _seq_name, diff_gt_poses_filename
-            )
-            self.seqs_info[_seq_name]["diff_kiss_poses_path"] = os.path.join(
-                seq_path, _seq_name, diff_kiss_poses_filename
             )
             if not (
                 len(os.listdir(self.seqs_info[_seq_name]["img0_path"]))
@@ -77,24 +76,73 @@ class KittiVLOdomDataset(Dataset):
                     "The number of point clouds and stereo images is not equal!"
                 )
 
+            calib_path = os.path.join(seq_path, _seq_name, calib_filename)
+            if not os.path.isfile(calib_path):
+                raise RuntimeError(f"Calibration file does not exist: {calib_path}!")
+            self.seqs_info[_seq_name]["calib"] = get_calibration(calib_path)
+
+            diff_kiss_poses_path = os.path.join(
+                seq_path, _seq_name, diff_kiss_poses_filename
+            )
+            if not os.path.isfile(diff_kiss_poses_path):
+                raise RuntimeError(
+                    f"Differential kiss poses do not exist: {diff_kiss_poses_path}!"
+                )
+            self.seqs_info[_seq_name]["diff_kiss_poses"] = np.load(diff_kiss_poses_path)
+
+            diff_gt_poses_path = os.path.join(
+                seq_path, _seq_name, diff_gt_poses_filename
+            )
+            if annotated and (not os.path.isfile(diff_gt_poses_path)):
+                raise RuntimeError(
+                    f"Differential gt poses do not exist: {diff_gt_poses_path}!"
+                )
+            self.seqs_info[_seq_name]["diff_gt_poses"] = (
+                np.load(diff_gt_poses_path) if annotated else None
+            )
+
             self.seqs_info[_seq_name]["len"] = (
                 len(os.listdir(self.seqs_info[_seq_name]["pcd_path"])) - 1
             )  # (len - 1) transforms per sequence
 
     def __len__(self):
-        return sum([_seq_info["len"] for _seq_info in self.seqs_info])
+        return sum([_seq_info["len"] for _seq_info in self.seqs_info.values()])
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        if self.__len__() <= idx or idx < 0:
+            raise IndexError("Index is out of range!")
 
-        img_name = os.path.join(self.root_dir, self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks], dtype=float).reshape(-1, 2)
-        sample = {"image": image, "landmarks": landmarks}
+        cumsum = 0
+        for _seq_info in self.seqs_info.values():
+            if idx >= cumsum and idx < cumsum + _seq_info["len"]:
+                break
+            cumsum += _seq_info["len"]
+        seq_idx = idx - cumsum
 
-        if self.transform:
-            sample = self.transform(sample)
+        img0_prev = io.imread(
+            os.path.join(_seq_info["img0_path"], str(seq_idx).zfill(6)) + ".png"
+        )
+        img1_prev = io.imread(
+            os.path.join(_seq_info["img1_path"], str(seq_idx).zfill(6)) + ".png"
+        )
+        pcd_prev = np.fromfile(
+            os.path.join(_seq_info["pcd_path"], str(seq_idx).zfill(6)) + ".bin",
+            dtype=np.float32,
+        ).reshape(-1, 4)
+        img0_curr = io.imread(
+            os.path.join(_seq_info["img0_path"], str(seq_idx + 1).zfill(6)) + ".png"
+        )
+        img1_curr = io.imread(
+            os.path.join(_seq_info["img1_path"], str(seq_idx + 1).zfill(6)) + ".png"
+        )
+        pcd_curr = np.fromfile(
+            os.path.join(_seq_info["pcd_path"], str(seq_idx).zfill(6)) + ".bin",
+            dtype=np.float32,
+        ).reshape(-1, 4)
 
-        return sample
+        return None
+
+
+if __name__ == "__main__":
+    dataset = KittiVLOdomDataset("data/SemanticKITTI/dataset")
+    print(dataset[4539])
--- odom_utils.py
+++ odom_utils.py
@@ -7,7 +7,7 @@ def check_spatial_transform(T: np.ndarray) -> None:
         raise ValueError("Non-4x4 matrix is not a spatial transformation!")
     if np.any(np.real(T) != T):
         raise ValueError("Only real numbers are supported in spatial transformations!")
-    if not np.allclose(T[:3, :3] @ T.T[:3, :3], np.identity(3),atol=1e-4):
+    if not np.allclose(T[:3, :3] @ T.T[:3, :3], np.identity(3), atol=1e-4):
         raise ValueError("Left-upper 3x3 matrix is not rotation!")
     if np.any(T[3, :] != np.array([0.0, 0.0, 0.0, 1.0])):
         raise ValueError("The footer must be [0 0 0 1]!")
@@ -22,3 +22,21 @@ def get_diff_transform(T_prev: np.ndarray, T_curr: np.ndarray) -> np.ndarray:
 def transform_to_pq(T: np.ndarray) -> np.ndarray:
     check_spatial_transform(T)
     return np.concatenate([T[:3, 3], R.from_matrix(T[:3, :3]).as_quat()])
+
+
+def get_calibration(calib_path: str):
+    with open(calib_path) as f:
+        lines = f.readlines()
+    obj = lines[0].strip().split(" ")[1:]
+    P0 = np.array(obj, dtype=np.float32)
+    obj = lines[1].strip().split(" ")[1:]
+    P1 = np.array(obj, dtype=np.float32)
+    obj = lines[4].strip().split(" ")[1:]
+    Tr_velo_to_cam = np.array(obj, dtype=np.float32)
+    return {
+        "P0": P0.reshape(3, 4),
+        "P1": P1.reshape(3, 4),
+        "K0": P0.reshape(3, 4)[:3, :3].copy(),
+        "K1": P1.reshape(3, 4)[:3, :3].copy(),
+        "Tr": Tr_velo_to_cam.reshape(3, 4),
+    }
